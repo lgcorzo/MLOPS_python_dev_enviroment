ceabf5dd2ae1bcada0f71862023b787768e9696b
L.corzo <l.corzo@lantek.com>
2023-07-26 00:49:28 +0200

    install angular

--- dev/entrypoint.sh
+++ dev/entrypoint.sh
@@ -6,10 +6,11 @@
 # init conda
 conda init
 # activate the environment
-conda activate code_develoment_env
+conda activate code_development_env
 
 # Now start the Angular development server
 ng serve --host 0.0.0.0
+#ng add @angular/material @angular/cdk
 
 # exec the cmd/command in this process, making it pid 1
 exec "$@"
